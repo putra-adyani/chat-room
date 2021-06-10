aaff86e4c5952d1efe988f1b6b7eb9e671c0c663
putra-adyani <sonyalfando12345@gmail.com>
2021-06-10 12:02:28 +0700

    add gem redis and modified cable.yml

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'landing/index'
 
-  root: 'landing#about'
+  # root: 'landing#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
