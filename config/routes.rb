2d919717770faed7003254881610f8d45f7b1699
putra-adyani <sonyalfando12345@gmail.com>
2021-06-10 16:24:17 +0700

    membuat tampilan chat-room dan menambahkan file stylesheets di folder javascript

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :rooms
   get 'landing/index'
 
+  root 'rooms#index'
   # root: 'landing#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
