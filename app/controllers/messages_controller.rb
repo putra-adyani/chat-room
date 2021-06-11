33031443e8b2c004b99dbda1e0f00e1eab4514a7
putra-adyani <sonyalfando12345@gmail.com>
2021-06-11 15:11:12 +0700

    setup action cable

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -24,7 +24,15 @@ class MessagesController < ApplicationController
     @message = Message.new(message_params)
     @message.user = current_user
     @message.save
-    redirect_to request.referrer
+    
+    # redirect_to request.referrer
+    html = render(
+      partial: 'messages/message',
+      locals: { message: @message}
+    )
+
+    ActionCable.server.broadcast("room_channel_#{@message.room_id}", {html: html})
+
   end
 
   # PATCH/PUT /messages/1 or /messages/1.json
